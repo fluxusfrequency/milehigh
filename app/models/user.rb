f75f37acc2dcae8151798f3bd69b98c15130d71c
Ben Lewis <bennlewis@gmail.com>
2014-01-05 13:01:33 -0700

    remove seedfinder meta tag and add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates_presence_of :full_name
+  validates_presence_of :display_name
+  validates_format_of   :email,
+                        :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates             :email, uniqueness: true
 end
