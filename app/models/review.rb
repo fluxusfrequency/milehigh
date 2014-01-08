6dd049b56aa95b0237e952e0537a31b76c064042
Ben Horne <benhorne44@gmail.com>
2014-01-08 16:47:18 -0700

    Adds validations to reviews for user id, store id, rating, body and title

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :store, class_name: Leafly::Store
+
+  validates :user_id, :store_id, :body, :title, presence: true
+  validates_inclusion_of :rating, :in => ['thumbs up', 'thumbs down']
 end
