1d58fb867412a694976255d0bba834c082322487
Bryana Knight <brknig11@gmail.com>
2014-01-09 13:15:56 -0700

    implement basic search for store name

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ mount Leafly::Engine, at: "leafly"
   get 'auth/:provider/callback' => 'sessions#create', only: %i(get post)
   get 'auth/failure', to: redirect('/')
   get 'signout' => 'sessions#destroy', only: %i(get delete), as: :signout
-
+  get '/search' => 'search#new'
 end
 
