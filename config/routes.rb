f6566c518a611af3dc0b11ea7fb4189ed3c8d055
Darryl Pequeen <pequickster@msn.com>
2014-01-08 19:14:06 -0700

    Started integrating newspaper theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,48 @@
 Milehigh::Application.routes.draw do
 
-  root 'welcome#index'
+  root 'dashboard#index'
 
   get 'auth/:provider/callback' => 'sessions#create', only: %i(get post)
   get 'auth/failure', to: redirect('/')
   get 'signout' => 'sessions#destroy', only: %i(get delete), as: :signout
 
+
+  # resources :articles, only: [:index, :show]
+
+  # get 'articles' => 'articles#index'
+  # get 'articles/new' => 'articles#new'
+  # get 'articles/:article' => 'articles#show'
+  # get 'articles/edit/:article' => 'articles#edit'
+  # post 'articles' => 'articles#create'
+  # put 'articles/:article' => 'articles#update'
+  # delete 'articles/:article' => 'articles#destroy'
+
 end
+
+
+# class ArticlesController < ActionController::Base
+#   def index
+#   end
+
+#   def new
+#   end
+
+#   def edit
+#   end
+
+#   def show
+#   end
+
+#   def create
+#     redirect_to articles_path == '/articles'
+#   end
+
+#   def update
+#     redirect_to article_path(@article) == '/articles/5'
+#   end
+
+#   def destroy
+#     redirect_to articles_path == '/articles'
+#   end
+
+# end
