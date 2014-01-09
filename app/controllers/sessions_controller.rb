681b49264b26301305b3719df8f0efb822088e6d
Ben Horne <benhorne44@gmail.com>
2014-01-09 13:46:07 -0700

    fixes signout path error when accessing engine pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
-  
+
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     user.find_avatar
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to home_path
   end
 
   def destroy
