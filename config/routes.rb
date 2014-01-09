91abbe70c8eb15f6884b9268b7c2bfb3e5ba0c51
Darryl Pequeen <pequickster@msn.com>
2014-01-09 10:35:00 -0700

    fixed bootstrap version problem

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ mount Leafly::Engine, at: "leafly"
 
   root 'welcome#index'
 
+  get 'home' => 'dashboard#index'
+
   get 'auth/:provider/callback' => 'sessions#create', only: %i(get post)
   get 'auth/failure', to: redirect('/')
   get 'signout' => 'sessions#destroy', only: %i(get delete), as: :signout
