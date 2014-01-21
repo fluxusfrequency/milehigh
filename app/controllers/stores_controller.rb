bb729d59f910ba33b7716ce960957fa42e66501a
Ben Lewis <bennlewis@gmail.com>
2014-01-21 11:04:25 -0700

    cleanup styling on store show page

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,6 +2,7 @@ class StoresController < ApplicationController
 
   def show
     @store = Store.find_by(slug: params[:slug])
+    @reviews = Review.where(:store_id => @store.id).limit(5)
     @strains = MenuStrain.by_store(@store)
   end
 
