26738dcda8d4a320b0c6f4d264fb4273bc830b6b
Ben Horne <benhorne44@gmail.com>
2014-01-22 16:15:15 -0700

    styles user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def show
     @thumbs_up_review_count = current_user.reviews.where(rating: "Thumbs Up").count
     @thumbs_down_review_count = current_user.reviews.where(rating: "Thumbs Down").count
+    @large_avatar = current_user.find_large_avatar
+    @friend_count = current_user.friends_on_milehigh.count
     @reviews = current_user.reviews
   end
 
