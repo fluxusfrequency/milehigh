c14c8706aa5d0c8598cd14a6be1d59d21cc3e67d
Bryana Knight <brknig11@gmail.com>
2014-01-22 09:20:22 -0700

    update user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @thumbs_up_review_count = current_user.reviews.where(rating: "Thumbs Up").count
     @thumbs_down_review_count = current_user.reviews.where(rating: "Thumbs Down").count
+    @reviews = current_user.reviews
   end
 
 end
